51d8fdc1dd70354c6406dd94e6e36c220025f655
code-curiosity <hitanshbhagtani@gmail.com>
2025-03-11 13:08:03 +0530

    Modified the way bot gives response to user

--- ruleBasedChatbot.py
+++ ruleBasedChatbot.py
@@ -14,11 +14,10 @@ def Load_intent(filenm = "intent.json"):
     except Exception as e:
         print(f"Something doesn't seem right 🫤 {e}")
         return [] #If the file is not found then it will return empty list, prevents crashing of program
-    
+intents = Load_intent() #Step:3 Load the intents from the json file
 def get_response(user_input):
     user_input = user_input.lower() #Step:1 Convert the user input to lower case for uniformity
     match_scores = {} #Step:2 Prepare dictionary for storing the matched scores of each intent
-    intents = Load_intent() #Step:3 Load the intents from the json file
     #Step:4 Loop through the intents
     for intent in intents:
         # Count how many patterns match the user input for each intent
@@ -31,20 +30,20 @@ def get_response(user_input):
         if match_count > 0:
             match_scores[intent["tag"]] = match_count
 
-        if not match_scores:
-        # Directly find 'unknown' tag responses here without a separate function
-            unknown_responses = next(intent["responses"] for intent in intents if intent["tag"] == "unknown")
-            return random.choice(unknown_responses)
+    if not match_scores:
+    # Directly find 'unknown' tag responses here without a separate function
+        unknown_responses = next(intent["responses"] for intent in intents if intent["tag"] == "unknown")
+        return random.choice(unknown_responses)
     
     best_match = max(match_scores, key=match_scores.get)  # Get the intent with most matches
     best_responses = next(intent["responses"] for intent in intents if intent["tag"] == best_match)
     
-    return random.choice(best_responses), best_match
+    return random.choice(best_responses)
 # Main loop
 # The chatbot will keep running until the user types anything that fully or partially matches the tag goodbye
 while True:
     user_input = input("You: ")
-    response, detected_tag = get_response(user_input)  # Get the response and the detected tag
+    response = get_response(user_input)  # Get the response and the detected tag
     print("Bot:", response)
     
     if detected_tag == "goodbye":  # If the detected tag is "goodbye", break the loop
